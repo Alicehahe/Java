5db97b812f9f4d144d8eed6ce6592ac2f9f4f681
=Jane <907309377@qq.com>
2020-03-25 14:25:53 +0800

    Java-注释练习

---
+++ Notes.java
@@ -0,0 +1,19 @@
+/**
+ * 这是文档注释，后续可用来生成API文档
+ * @Author：HanLu
+ * @Description:
+ * @Date:Created in 3:11 PM 2020/3/23
+*/
+
+public class Notes{
+    public static void main(String[] args) {
+        System.out.println("helllo world"); //这是单行注释，用的比较多
+
+        /*
+        这是多行注释，一般用于说明比较复杂逻辑
+         */
+
+        System.out.println("ksdhggit");
+    }
+}
+
